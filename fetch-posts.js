7477479e7243b5343769a062d248418c6b237bb4
griffmonster <griff.chamberlain@gmail.com>
2026-02-24 16:15:49 +0000

    Create fetch-posts.js
    fetch posts script initial commit

---
+++ fetch-posts.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+async function fetchAllEntries() {
+            let allEntries = [];
+            let currentIndex = 1; 
+            let keepFetching = true;
+            const stats = document.getElementById('stats');
+			const blogUrl = "https://griffmonster-walks.blogspot.com";
+
+            while (keepFetching) {
+                try {
+                    const response = await fetch(`${blogUrl}/feeds/posts/default?alt=json&start-index=${currentIndex}&max-results=150`);
+                    const data = await response.json();
+                    const entries = data.feed.entry;
+
+                    if (entries && entries.length > 0) {
+                        allEntries = allEntries.concat(entries);
+                        currentIndex += entries.length;
+                        stats.innerHTML = `Scanning post metadata... (${allEntries.length} found)`;
+                    } else {
+                        keepFetching = false;
+                    }
+                } catch (e) {
+                    keepFetching = false;
+                }
+            }
+            // Save the result to a file
+		  fs.writeFileSync('data.json', JSON.stringify(allEntries));
+		  console.log(`Success! Saved ${allEntries.length} posts.`);
+        }
+
+fetchAllEntries();
